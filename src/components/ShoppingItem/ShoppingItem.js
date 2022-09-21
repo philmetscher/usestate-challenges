ec719f1b9bfa282b2fde64055f0f7fe5165794e9
Phil Metscher <philmetschermailt@gmx.de>
2022-09-21 11:57:05 +0200

    add shopping item functionality

--- src/components/ShoppingItem/ShoppingItem.js
+++ src/components/ShoppingItem/ShoppingItem.js
@@ -1,18 +1,32 @@
+import { useState } from "react";
 import "./ShoppingItem.css";
 
 function ShoppingItem({ name, price }) {
-  const amount = 1;
+  let [amount, setAmount] = useState(1);
 
+  const prevAmount = () => amount;
   return (
     <section className="item">
       <h2>{name}</h2>
-      <p>price: {price}</p>
+      <p>price: {price.toFixed(2)}€</p>
       <p>amount: {amount}</p>
-      <p>total: {price * amount}</p>
-      <button className="item__button" type="button">
+      <p>total: {(price * amount).toFixed(2)}€</p>
+      <button
+        className="item__button"
+        type="button"
+        onClick={() => {
+          amount != 1
+            ? setAmount((prevAmount) => prevAmount - 1)
+            : alert("that's the min!");
+        }}
+      >
         -
       </button>
-      <button className="item__button" type="button">
+      <button
+        className="item__button"
+        type="button"
+        onClick={() => setAmount((prevAmount) => prevAmount + 1)}
+      >
         +
       </button>
     </section>
