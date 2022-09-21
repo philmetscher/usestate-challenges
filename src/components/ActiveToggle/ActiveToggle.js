e535ecc8fdc60db7cf396804eaf65d9ae22e3366
Phil Metscher <philmetschermailt@gmx.de>
2022-09-21 11:34:54 +0200

    add toggle event for box

--- src/components/ActiveToggle/ActiveToggle.js
+++ src/components/ActiveToggle/ActiveToggle.js
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import "./ActiveToggle.css";
 
 function ActiveToggle() {
+  const [active, setActive] = useState(true);
   return (
     <main>
-      <div className="box" />
-      <button type="button">Activate</button>
+      <div className={"box" + (active ? " box--active" : "")} />
+      <button
+        type="button"
+        onClick={() => {
+          setActive(!active);
+        }}
+      >
+        {active ? "Activate" : "Deactivate"}
+      </button>
     </main>
   );
 }
